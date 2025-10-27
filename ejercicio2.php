a0a5f7cd2aa8196e29671bd492a373cf54c6bc76
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-10-27 10:12:16 +0100

    segundo ejercicio

---
+++ ejercicio2.php
@@ -0,0 +1,15 @@
+<?php
+ $capitales = [
+    ['country' => "japan"],
+    ['country' => "mexico"],
+    ['country' => "usa"],
+    ['country' => "india"],
+    ['country' => "korea"],
+    ['country' => "china"],
+    ['country' => "nigeria"],
+    ['country' => "argentina"],
+    ['country' => "egypt"],
+    ['country' => "england"]
+
+];
+ var_dump($capitales);
