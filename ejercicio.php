6bfed2cbb97852a93779f0d67511c63153b96ed3
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-10-27 09:48:56 +0100

    primer ejercicio

--- ejercicio.php
+++ ejercicio.php
@@ -1,1 +1,12 @@
 <?php
+$capitales=[
+    ['city'=>"tokyo"],
+    ['city'=>"mexico city"],
+    ['city'=>"mumbai"],
+    ['city'=>"seoul"],
+    ['city'=>"lagos"],
+    ['city'=>"buenos aires"],
+    ['city'=>"cairo"],
+    ['city'=>"london"]
+];
+ var_dump($capitales);
