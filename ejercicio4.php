2a99a97e978a3ec983809a8e6e5c4e99ff196957
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-10-27 11:24:11 +0100

    mitad de ejercicio 4

---
+++ ejercicio4.php
@@ -0,0 +1,26 @@
+<?php
+$paises=[
+    [
+        'city'=>"tokyo"
+        'country'=>"japan"
+    ],
+    [
+        'city'=>"mexico city"
+        'country'=>"mexico"
+    ],
+    [
+        'city'=>"new york"
+        'country'=>"USA"
+    ],
+    [
+        'city'=>"mumbai"
+        'country'=>"india"
+    ],
+    [
+        'city'=>"seoul"
+        'country'="india"
+    ],
+    [
+        'city'
+    ]
+];
