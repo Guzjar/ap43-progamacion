0bd325b10e88584ad6802f625b4d6e84b70c6b4d
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-10-31 09:29:56 +0100

    ejercicio 5

--- ejercicio5.php
+++ ejercicio5.php
@@ -1,2 +1,13 @@
 <?php 
-function(explode)
+$data="Tokyo, Japan; Mexico City, Mexico; New York City, USA; Mumbai, India; Seoul, Korea; Shanghai, China; Lagos, Nigeria; Buenos Aires, Argentina; Cairo, Egypt; London, England.";
+$pais=explode(";"$data);
+$mundo=[];
+foreach ($data as $pais) {
+    $partes=explode(",",$par);
+    if(count($partes)==2){
+        $ciudad=$partes[0];
+        $pais=$partes[1];
+        $mundo[$ciudad]=$pais;
+    }
+}
+var_dump($mundo);
